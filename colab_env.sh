8d8ee9dcf24c56f46e93033ec76d9a65c31b416b
Adscn <theadscn@gmail.com>
2025-10-01 15:21:25 +0000

    add for colab env

---
+++ colab_env.sh
@@ -0,0 +1,5 @@
+export PIP_CACHE_DIR=/content/drive/MyDrive/.pip-cache
+mkdir -p "$PIP_CACHE_DIR"
+pip install --index-url https://download.pytorch.org/whl/cu124 \
+  torch torchvision torchaudio --upgrade
+pip install -r requirements_colab.txt
